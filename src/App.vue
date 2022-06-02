f4d5b700719d15daf49a2b49d86cc0e4b681b0eb
Samuel <samuel.louismarie@gmail.com>
2022-06-02 09:59:37 +0200

    compteur avec bouton

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,22 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>
+  import {ref} from 'vue'
+  //Increment a counter every time the button is clicked
+  //Modify the counter value by writing in the input field
+  //Reset the counter by clicking the button
+  const counter = ref(0);
+  const increment = () => counter.value++;  
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a simple example of a Vue.js component.</p>
+    <p>{{counter}}</p>
+    <button @click="increment">Increment</button>
+    <input v-model="counter" type="number" />
+    <button @click="counter = 0">Reset</button>
+  </div> 
 </template>
 
 <style>
-@import './assets/base.css';
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
 </style>
