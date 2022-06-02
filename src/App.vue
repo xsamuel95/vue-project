88dec419890a432b4b66be576a78019a590dbd72
Samuel <samuel.louismarie@gmail.com>
2022-06-02 10:22:08 +0200

    compteur fonctionnel avec reset button

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <script setup>
-  import {ref} from 'vue'
-  //Increment a counter every time the button is clicked
-  //Modify the counter value by writing in the input field
-  //Reset the counter by clicking the button
-  const counter = ref(0);
-  const increment = () => counter.value++;  
+  import {ref} from "vue";
+  const compteur = ref(0);
+  setInterval( () => {
+    compteur.value++;
+  }, 1000 )
+  function resetCompteur() {
+    compteur.value = 0;
+  }
 </script>
 
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <p>compteur</p>
-    <p>{{counter}}</p>
-    <button @click="increment">Incrementer</button> <br>
-    <input v-model="counter" type="number" />
-    <button @click="counter = 0">Reset</button>
-  </div> 
+
+  <main>
+    <h1>Hello world</h1>
+
+    <p>
+      compteur : {{ compteur }}
+    </p>
+
+    <div>
+      <button @click="resetCompteur()">Reset compteur</button>
+
+      <input type="number" v-model="compteur">
+    </div>
+
+  </main>
+
 </template>
 
-<style>
-</style>
+<style></style>
