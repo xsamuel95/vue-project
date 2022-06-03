6c8e8d8316606874971c0b616b24a89d0d6f7a7e
Samuel <samuel.louismarie@gmail.com>
2022-06-03 10:48:53 +0200

    modif du App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 App.vue>style>@import 'bootstrap/dist/css/bootstrap.min.css'; 
 import compteur from './components/compteur.vue';
 import tableau from './components/tableau.vue';
+import Connexion from './components/Connexion.vue';
+import Inscription from './components/Inscription.vue';
+
 </script>
 
 <template>
