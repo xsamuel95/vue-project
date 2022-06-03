f756592cbdbe841fe7b3b2c3ffb9f4d299544c00
Samuel <samuel.louismarie@gmail.com>
2022-06-03 11:01:23 +0200

    modif du main avec navbar don't work !!

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <script setup>
-App.vue>style>@import 'bootstrap/dist/css/bootstrap.min.css'; 
 import compteur from './components/compteur.vue';
 import tableau from './components/tableau.vue';
 import Connexion from './components/Connexion.vue';
 import Inscription from './components/Inscription.vue';
-
 </script>
 
 <template>
-    <Compteur></Compteur>
-    <DemoJWT></DemoJWT>
-    <LoopTabVue></LoopTabVue>
+
+  <main class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary px-2">
+      <span class="navbar-brand" v-if="user">Hey {{ user.name }}</span>
+      <span class="navbar-brand" v-else>Welcome</span>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+          </ul>
+
+          <ul class="navbar-nav mr-auto">
+        <li v-if="!user" class="nav-item">
+          <RouterLink class="nav-link btn btn-success" :to="{name:'connexion'}">Connexion</RouterLink>
+        </li>
+
+        <li v-if="!user" class="nav-item m-2 mt-0 mb-0">
+          <RouterLink class="nav-link btn btn-warning" :to="{name:'inscription'}">S'inscrire</RouterLink>
+        </li>
+    <RouterView />
+    </main>
 </template>
 
 <style>
